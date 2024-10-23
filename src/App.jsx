4396b271ab654cced9b5ee959c63596e5bdb9927
MursalinDevOps <mursalin1768@gmail.com>
2024-10-23 16:26:40 +0600

    Prepare btn's functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,36 @@ import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
   const [recipeQueue, setRecipeQueue] = useState([]);
+  const [prepareRecipe, setPrepareRecipe] = useState([]);
+
 
 
   const handleRecipe = (recipe) => {
     const doesExists = recipeQueue.find(prevRes => prevRes.recipe_id === recipe.recipe_id);
-    if(!doesExists){
+    if (!doesExists) {
       setRecipeQueue([...recipeQueue, recipe])
-    }else{
+    } else {
       alert('This recipe already exists in the queue!')
     }
 
+    
+  }
+  const handleRemove = (id) => {
+    const deletedRecipe = recipeQueue.find(recipe => recipe.recipe_id === id)
+
+    const updatedQueue = recipeQueue.filter(recipe => recipe.recipe_id !== id)
+    setRecipeQueue(updatedQueue)
+    setPrepareRecipe([...prepareRecipe, deletedRecipe])
   }
   return (
     <>
-     <Navbar></Navbar>
-     <Banner></Banner>
-     <OurRecipes></OurRecipes>
-     <div className='md:flex justify-between'>
-      <Cards handleRecipe={handleRecipe}></Cards>
-      <Sidebar recipeQueue={recipeQueue}></Sidebar>
-     </div>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <OurRecipes></OurRecipes>
+      <div className='md:flex justify-between'>
+        <Cards handleRecipe={handleRecipe}></Cards>
+        <Sidebar prepareRecipe={prepareRecipe} handleRemove={handleRemove} recipeQueue={recipeQueue}></Sidebar>
+      </div>
     </>
   )
 }
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-export default function Sidebar( {recipeQueue} ) {
+export default function Sidebar( {recipeQueue, handleRemove, prepareRecipe} ) {
   return (
     <div className="md:w-1/3 my-8  ml-6 border-2 rounded-2xl">
       <h3 className="text-center text-xl my-4 ">Want to cook : {recipeQueue.length}</h3>
@@ -26,14 +26,16 @@ export default function Sidebar( {recipeQueue} ) {
         <td>{recipe.preparing_time}</td>
         <td>{recipe.calories}</td>
         <td colSpan='2'> 
-          <button className="btn bg-[#0BE58A] rounded-3xl">Preparing</button>
+          <button
+          onClick={()=>handleRemove(recipe.recipe_id)} 
+          className="btn bg-[#0BE58A] rounded-3xl">Preparing</button>
         </td>
       </tr>)
       }
     </tbody>
   </table>
 
-  <h3 className="border-y py-4 text-center text-xl my-4 ">Preparing : 0000</h3>
+  <h3 className="border-y py-4 text-center text-xl my-4 ">Preparing : {prepareRecipe.length}</h3>
 
   {/* Table 2 */}
   <table className="table">
@@ -48,17 +50,22 @@ export default function Sidebar( {recipeQueue} ) {
     </thead>
     <tbody>
       {/* row 1 */}
-      {/* {
-          recipeQueue.map((recipe, idx)=> <tr key={idx}>
+       {
+          prepareRecipe.map((recipe, idx)=> <tr key={idx}>
         <td>{idx + 1}</td>
         <td>{recipe.recipe_name}</td>
         <td>{recipe.preparing_time}</td>
         <td>{recipe.calories}</td>
-        <td colSpan='2'> 
-          <button className="btn bg-[#0BE58A] rounded-3xl">Preparing</button>
-        </td>
       </tr>)
-      } */}
+      } 
+
+      {/* Sum */}
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
     </tbody>
   </table>
 </div>
