78fcf5543ab55a0b106abe4553770d1fbafd2d9a
MursalinDevOps <mursalin1768@gmail.com>
2024-10-23 11:05:11 +0600

    Navigation bar added and has been made responsive

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,43 @@
+
+export default function Navbar() {
+    return (
+        <nav className='flex flex-col md:flex-row justify-around items-center gap-4 md:gap-0 my-3'>
+            {/* Logo */}
+            <div>
+                <h3 className="text-lg font-bold">The Chefs Table</h3>
+            </div>
+            {/* Nav Items */}
+            <div>
+                <ul className='flex gap-5'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Recipes</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Search</a></li>
+                </ul>
+            </div>
+            {/* Search & Profile */}
+            <div className='flex items-center gap-3'>
+                <div>
+                    <label className="input input-bordered flex items-center gap-2">
+                        <input type="text" className="grow" placeholder="Search" />
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4 opacity-70">
+                            <path
+                                fillRule="evenodd"
+                                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </label>
+                </div>
+                <div className="h-10 w-10 rounded-full bg-[#0BE58A] flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                </div>
+            </div>
+        </nav>
+    )
+}
